6e3dfd80dcdb6513578da9d9d0c0cc13d3fd2f06
Vlad <paliy.work@gmail.com>
2019-11-14 17:40:00 +0200

    Using views engine

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-
 const app = express();
+const hbs = require("hbs");
+const expressHbs = require("express-handlebars");
 
-const urlencodedParser = bodyParser.urlencoded({extended: false});
+// app.engine("hbs", expressHbs({
+//   layoutsDir: "views/layouts",
+//   defaultLayout: "layout",
+//   extname: "hbs"
+// }))
 
-app.get("/register", urlencodedParser, function (request, response) {
-  response.sendFile(__dirname + "/register.html");
+hbs.registerHelper("getTime", function(){
+     
+  var myDate = new Date();
+  var hour = myDate.getHours();
+  var minute = myDate.getMinutes();
+  var second = myDate.getSeconds();
+  if (minute < 10) {
+      minute = "0" + minute;
+  }
+  if (second < 10) {
+      second = "0" + second;
+  }
+  return "Текущее время: " + hour + ":" + minute + ":" + second;
 });
 
-app.post("/register", urlencodedParser, function(request, response){
+hbs.registerHelper("createStringList", function(array){
+     
+  var result="";
+  for(var i=0; i<array.length; i++){
+      result +="<li>" + array[i] + "</li>";
+  }
+  return new hbs.SafeString("<ul>" + result + "</ul>");
+});
 
-  if(!request.body) return response.sendStatus(400);
+app.set("view engine", "hbs");
+//app.set("views", "views");
+//hbs.registerPartials(__dirname + "/views/partials");
 
-  console.log(request.body);
+app.use("/contact", function(request, response){
 
-  response.send(`${request.body.userName} - ${request.body.userAge}`);
+  response.render("contact.hbs", {
+    title: "Мои контакты",
+    emailsVisible: true,
+    emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+    phone: "+1234567890"
+});
 
-})
+});
 
-app.get("/", function(request, response){
-  response.send("main page")
-})
+app.use("/", function(request, response){
 
-app.listen(3000)
+  response.render("home.hbs", { 
+    fruit: [ "apple", "lemon", "banana", "grape"]
+  });
+
+});
+
+app.listen(3000);
